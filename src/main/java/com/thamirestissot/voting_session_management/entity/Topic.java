feb82a50eeb7f7541ea329159f96086c58847584
thamirestissot <thamirestissot3@gmail.com>
2020-05-23 20:50:50 -0300

    Cria métodos Get's e Set's na entidade Topic

--- src/main/java/com/thamirestissot/voting_session_management/entity/Topic.java
+++ src/main/java/com/thamirestissot/voting_session_management/entity/Topic.java
@@ -16,4 +16,27 @@ public class Topic {
     private String title;
     private String description;
 
+    public List<VotingSession> getVotingSessions() {
+        return votingSessions;
+    }
+
+    public void setVotingSessions(List<VotingSession> votingSessions) {
+        this.votingSessions = votingSessions;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }

7979db99cedfe08364a5484a3cd75aac2242cb4c
thamirestissot <thamirestissot3@gmail.com>
2020-05-23 19:28:55 -0300

    Adiciona interface AssociateRepository

--- src/main/java/com/thamirestissot/voting_session_management/repository/AssociateRepository.java
+++ src/main/java/com/thamirestissot/voting_session_management/repository/AssociateRepository.java
@@ -1,4 +1,9 @@
 package com.thamirestissot.voting_session_management.repository;
 
-public class AssociateRepository {
+import com.thamirestissot.voting_session_management.entity.Associate;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("associateRepository")
+public interface AssociateRepository extends JpaRepository<Associate, Integer> {
 }

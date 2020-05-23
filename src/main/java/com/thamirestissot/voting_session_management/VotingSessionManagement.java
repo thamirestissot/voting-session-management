8159340bfeeaa1ba20cc7cc77736990b1fb066ce
thamirestissot <thamirestissot3@gmail.com>
2020-05-23 19:24:39 -0300

    Cria classe principal
    Cria classe correta

---
+++ src/main/java/com/thamirestissot/voting_session_management/VotingSessionManagement.java
@@ -0,0 +1,12 @@
+package com.thamirestissot.voting_session_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingSessionManagement {
+    public static void main(String[] args) {
+        SpringApplication.run(VotingSessionManagement.class, args);
+    }
+
+}
